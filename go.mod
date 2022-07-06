f6b7aa2c67278e5de373e980c88c03d6dc600116
Katya Safford <katerin2011@gmail.com>
2022-07-06 16:43:12 -0500

    with middleware

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module myapp
 
 go 1.14
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
+)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"myapp/pkg/config"
+	"myapp/pkg/models"
 	"myapp/pkg/render"
 	"net/http"
 )
@@ -11,18 +12,6 @@ type Repository struct {
 }
 
 var Repo *Repository
-
-// holds data sent from handlers to templates
-// type TemplateData struct {
-// 	StringMap map[string]string
-// 	IntMap    map[string]int
-// 	FloatMap  map[string]float32
-// 	Data      map[string]interface{}
-// 	CSRFToken string
-// 	Flash     string
-// 	Warning   string
-// 	Error     string
-// }
 
 // creates a new repository
 func NewRepo(a *config.AppConfig) *Repository {
@@ -37,9 +26,14 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	stringMap := make(map[string]string)
+	stringMap["test"] = "hello again"
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
